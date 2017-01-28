948acc90cd68f15064a21bab2aed0264d066e48b
vladimir <coldsice@gmail.com>
2017-01-28 16:39:42 +0300

    root component design update try 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+	isMenuOpen:boolean = false;
+
+	_openIconMenu() {
+		document.getElementById('gn_menu_wrapper').classList.add('gn-open-part'); 
+	}
+
+	_closeIconMenu() {
+		document.getElementById('gn_menu_wrapper').classList.remove('gn-open-part');
+	}
+
+	_openMenu() {
+		if( this.isMenuOpen ) return;
+		document.getElementById('gn_icon_menu').classList.add('gn-selected'); 
+		this.isMenuOpen = true;
+		document.getElementById('gn_menu_wrapper').classList.add('gn-open-all');
+		this._closeIconMenu();
+	}
+
+
+	_closeMenu() {
+		if( !this.isMenuOpen ) return;
+		document.getElementById('gn_icon_menu').classList.remove('gn-selected'); 
+		this.isMenuOpen = false;
+		document.getElementById('gn_menu_wrapper').classList.remove('gn-open-all');
+		this._closeIconMenu();
+	}
+	
+
+	bodyClickFn() {
+		this._closeMenu();
+	}
+
+
+	_toggleMenu(){
+		if(this.isMenuOpen){
+			this._closeMenu();
+		}else{
+			this._openMenu();
+		}
+	}
+
 }
